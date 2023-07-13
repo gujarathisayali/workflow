656e4e173bef439d700fa8a543ad1def37a9c2d1
Sayali Gujarathi <sayali.gujarathi@impetus.com>
2023-07-13 14:13:43 +0530

    file added

---
+++ dbscripts/V4.1.1__objects.sql
@@ -0,0 +1,16 @@
+Create or Replace table demo.test.WEATHER1(
+    Precipitation Number(5,3),
+    Date varchar(10),
+    Month Number(2),
+    Week Number(2),
+    Year Number(4),
+    City Varchar(100),
+    Code Varchar(4),
+    Location Varchar(100),
+    State Varchar(100),
+    Avg_Temp Number(3),
+    Max_Temp Number(3),
+    Min_Temp Number(3),
+    Wind_Direction Number(3),
+    Wind_Speed Number(5,2)
+    );
